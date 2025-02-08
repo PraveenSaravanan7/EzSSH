2b49b7fab7e9c7c8bb4d59480d8276bc3ef10cf7
Praveen Saravanan <praveensaravananoff@gmail.com>
2025-02-08 22:16:52 +0530

    init

---
+++ src/ui/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main"></div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <button
+        id="close"
+        onClick={() => window.electron.sendFrameAction("CLOSE")}
+      />
+      <button
+        id="minimize"
+        onClick={() => window.electron.sendFrameAction("MINIMIZE")}
+      />
+      <button
+        id="maximize"
+        onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
+      />
+    </header>
+  );
+}
+
+export default App;
