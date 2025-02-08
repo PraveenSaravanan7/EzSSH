15ef2c5bf9074d2c4f2155443ea3cd8160a26788
Praveen Saravanan <praveensaravananoff@gmail.com>
2025-02-08 22:46:35 +0530

    Added add button

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="app"></div>;
+  const [showAddScreen, setShowAddScreen] = useState(false);
+
+  return (
+    <div className="app">
+      <div className="itemContainer">
+        <div></div>
+        <div>
+          <button
+            onClick={() => setShowAddScreen((prev) => !prev)}
+            className="button"
+            style={{ width: "100%" }}
+          >
+            + Add
+          </button>
+        </div>
+      </div>
+      {showAddScreen ? <Add></Add> : <div className="terminal"></div>}
+    </div>
+  );
+};
+
+const Add = () => {
+  return (
+    <div>
+      <h2>Add</h2>
+    </div>
+  );
 };
 
 export default App;
