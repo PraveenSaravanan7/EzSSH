17b932cde260c2ceb563e9517f8abea69198b0ec
Praveen Saravanan <praveensaravananoff@gmail.com>
2025-03-02 17:51:02 +0530

    Added welcome

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import { ConnectionList } from "./components/ConnectionList";
 import { Header } from "./components/Header";
+import { AddConnection } from "./components/AddConnection";
+import { Terminal } from "./components/Terminal";
 
 const App = () => {
+  const [showAddConnection, setShowAddConnection] = useState(false);
+  const [activeConnection, setActiveConnection] = useState("");
+
   return (
     <>
       <Header />
       <div className="app">
         <ConnectionList />
+
+        {showAddConnection && <AddConnection />}
+        {!!activeConnection && <Terminal />}
+        {!showAddConnection && !activeConnection && <WelcomeMessage />}
       </div>
     </>
+  );
+};
+
+const WelcomeMessage = () => {
+  return (
+    <div className="welcomeMessage">
+      <h1>EzSSH</h1>
+    </div>
   );
 };
 
---
+++ src/ui/components/AddConnection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddConnection = () => {
+  return <div>AddConnection</div>;
+};
