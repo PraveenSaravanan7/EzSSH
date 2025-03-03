05227f7e3bdfee24041700a2a19aea56b575705e
Praveen Saravanan <praveensaravananoff@gmail.com>
2025-03-04 00:17:42 +0530

    Added layout in add connection

--- src/ui/components/AddConnection.tsx
+++ src/ui/components/AddConnection.tsx
@@ -10,21 +10,29 @@ export const AddConnection = ({ onCancel }: IAddConnectionProps) => {
       <h2>New Connection</h2>
 
       <div className="connectionConfigForm">
-        <div>
-          <div className="label">Host</div>
-          <input type="text" />
+        <div className="group">
+          <div>
+            <div className="label">Host</div>
+            <input style={{ width: "300px" }} type="text" />
+          </div>
+          <div>
+            <div className="label">Port</div>
+            <input style={{ width: "100px" }} type="number" />
+          </div>
+        </div>
+        <div className="group">
+          <div>
+            <div className="label">Username</div>
+            <input type="text" />
+          </div>
+          <div>
+            <div className="label">Password</div>
+            <input type="text" />
+          </div>
         </div>
         <div>
-          <div className="label">Port</div>
-          <input type="number" />
-        </div>
-        <div>
-          <div className="label">Username</div>
-          <input type="text" />
-        </div>
-        <div>
-          <div className="label">Password</div>
-          <input type="text" />
+          <div className="label">Key file</div>
+          <input style={{ width: "424px" }} type="file" />
         </div>
       </div>
 
